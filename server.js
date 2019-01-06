52fcbd09dcfba04b91b29c40ef13e3996b67bf91
Robert Flanagan <robertflanagan95@gmail.com>
2019-01-06 17:04:09 -0500

    html placeholders to test routing

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+  
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
