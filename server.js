51918e9ad96c0ae5b2b2f65a433249600f2c5412
Robert Flanagan <robertflanagan95@gmail.com>
2019-01-06 17:48:30 -0500

    testing routes

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname+"app/public/", "survey.html"));
+    res.sendFile(__dirname+"/app/public/survey.html");
 });
   
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname+"app/public/", "home.html"));
+    res.sendFile(__dirname+"/app/public/home.html");
 });
 
 app.listen(PORT, function() {
