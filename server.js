86a21b89c5be1536d4bddf9b573094ea9d035a56
Robert Flanagan <robertflanagan95@gmail.com>
2019-01-06 18:25:49 -0500

    htmlRoutes working

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get("/survey", function(req, res) {
-    res.sendFile(__dirname+"/app/public/survey.html");
-});
-  
-app.get("/", function(req, res) {
-    res.sendFile(__dirname+"/app/public/home.html");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
