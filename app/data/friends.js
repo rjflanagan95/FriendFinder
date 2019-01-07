4d439c264ccf339bd41e61740b68a6b577d02474
Robert Flanagan <robertflanagan95@gmail.com>
2019-01-06 22:43:24 -0500

    compatibility logic added, still printing to console

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendArray = [
     {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        friendName: "Ahmed",
+        friendPhoto: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [
             5,
             1,
